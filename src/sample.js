3390e89c1e4152caee66dc31bc1e2448206834c4
yessine-rebhi <rabhiyassin@gmail.com>
2024-12-26 00:42:37 +0100

    Testing the Cleanup

---
+++ src/sample.js
@@ -0,0 +1,38 @@
+// This is a sample file with some unused code and unnecessary comments.
+
+// Unused variables
+const unusedVar = "I am unused!";
+let anotherUnusedVar = 42;
+
+// Function with unnecessary comments
+/**
+ * This is a sample function.
+ * It does something useful, but it's commented too much.
+ */
+function exampleFunction() {
+  console.log("This function does something!");
+  // This is a debug log, it shouldn't be here
+  console.log("Debug log for something!");
+  const temp = "Temporary variable";
+  return temp;
+}
+
+// A block of code that's completely unnecessary and should be cleaned up
+function unusedFunction() {
+  console.log("This function does nothing!");
+}
+
+// Here we have an async function that should also be cleaned
+async function fetchData() {
+  console.log("Fetching data...");
+  // Unused promise here
+  return new Promise((resolve, reject) => {
+    resolve("Data fetched!");
+  });
+}
+
+// Unnecessary imports
+import { someUnusedLibrary } from 'some-library';
+
+// More unused variables and code
+let unusedCode = "Don't need me!";
