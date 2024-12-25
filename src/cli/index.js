d964ac620896dbdeff7063332d4f9a68372a44c8
yessine-rebhi <rabhiyassin@gmail.com>
2024-12-26 00:02:35 +0100

    Automated cleanup by nothing-happened

--- src/cli/index.js
+++ src/cli/index.js
@@ -6,7 +6,7 @@ const program = new Command();
 program
   .name('nothing-happened')
   .description('Automated code cleanup for your repository')
-  .version('0.1.0');
+  .version('1.0.0');
 
 program
   .command('cleanup')
